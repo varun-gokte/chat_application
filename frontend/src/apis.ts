4f3945828ba0c18e5e49d64a1210b84a3ceec5ac
varun-gokte <vsgokte@gmail.com>
2026-07-20 16:07:53 +0530

    changes

--- frontend/src/apis.ts
+++ frontend/src/apis.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosError} from "axios";
 
-const URL = "http://localhost:3000/api"
+const URL = "https://chat-application-4an4.onrender.com/api"
 
 const signupUser = async (data: {firstName:string, lastName:string, username:string, password:string}) => {
   try{
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://chat-application-4an4.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect:false,
