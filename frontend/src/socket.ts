f1bc4f8be8164f032e4290eb924043a07e0eeaf7
varun-gokte <vsgokte@gmail.com>
2026-08-05 17:16:45 +0530

    changes

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const SOCKET_URL = "https://chat-app-varun.vercel.app";
+const SOCKET_URL = "https://chat-application-4an4.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect:false,
